a2ae4778ae85730b1690cd455ec19e57cebcbcf3
parveen999 <44468071+parveen999@users.noreply.github.com>
2019-01-14 16:54:18 -0500

    week 2-Class Hand in work

---
+++ script.js
@@ -0,0 +1,15 @@
+// Name:Parveen kaur
+// Date:January 14,2019
+//Student:c0743933
+//Course: CSD 2214 sECTION 6
+
+let clock;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+console.log(endDate.value); 
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+//console.log(endDate);
